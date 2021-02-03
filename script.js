36b46e5ad54b1655496f14e8997e265c11799ebc
Jolan Callens <jolan.callens@gmail.com>
2021-02-03 10:55:36 +0100

    avancement cursor

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-let cursor = document.getElementById('cursor')
+const cursor = document.getElementById('cursor')
+const elementHover = document.querySelectorAll('.hover-this')
 
 window.addEventListener('mousemove', (e) => {
-  cursor.style.left = e.clientX - 50/2 + 'px'
-  cursor.style.top = e.clientY - 50/2 + 'px'
+  cursor.style.left = e.clientX - 30 / 2 + 'px'
+  cursor.style.top = e.clientY - 30 / 2 + 'px'
 })
 
 console.log(cursor)
