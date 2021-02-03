4fe64b70367c3225d14b828e781b21916d827608
benji22ben <benji22ben@gmail.com>
2021-02-03 09:12:49 +0100

    TEST

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ let cursor = document.getElementById('cursor')
 
 window.addEventListener('mousemove', (e) => {
   console.log(e);
+  cursor.style.transform.translateX = e.clientX;
+  cursor.style.transform.translateY = e.clientY;
 })
