25b9f386d679c987c66f86af012336503a062aa7
Jolan Callens <jolan.callens@gmail.com>
2021-02-03 10:34:24 +0100

    modifs

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let cursor = document.getElementById('cursor')
 
 window.addEventListener('mousemove', (e) => {
-  cursor.style.left = e.clientX + 50/2 + 'px'
-  cursor.style.top = e.clientY + 50/2 + 'px'
+  cursor.style.left = e.clientX - 50/2 + 'px'
+  cursor.style.top = e.clientY - 50/2 + 'px'
 })
 
 console.log(cursor)
