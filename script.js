6b939b0a5c68b9d005c2279c7a60249ff05104e8
Jolan Callens <jolan.callens@gmail.com>
2021-02-09 19:06:42 +0100

    tah les modifs

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 const cursor = document.querySelector('.cursor')
 const elementHover = document.querySelectorAll('.hover-this')
 
-// window.addEventListener('mousemove', (e) => {
-//   cursor.style.left = e.clientX - 30 / 2 + 'px'
-//   cursor.style.top = e.clientY - 30 / 2 + 'px'
-// })
 
 document.addEventListener('mousemove', (e) => {
     cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
@@ -17,5 +13,3 @@ document.addEventListener('click', () => {
         cursor.classList.remove("expand");
     }, 500)
 })
-
-console.log(cursor)
